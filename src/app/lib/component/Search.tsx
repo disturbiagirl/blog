5bde2f4ddfdecc14c72f7fc687f8032afd24999c
Anastasia <122746649+disturbiagirl@users.noreply.github.com>
2024-02-02 23:32:28 +0100

    capture users input

--- src/app/lib/component/Search.tsx
+++ src/app/lib/component/Search.tsx
@@ -1,10 +1,25 @@
+"use client";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import {
   faMagnifyingGlass,
   faCircleStop,
 } from "@fortawesome/free-solid-svg-icons";
+// import { useSearchParams, usePathname, useRouter } from "next/navigation";
 
 const Search = () => {
+  // const searchParams = useSearchParams();
+  // const pathname = usePathname();
+  // const { replace } = useRouter();
+  function handleSearch(term: string) {
+    // const params = new URLSearchParams(searchParams);
+    console.log(term);
+    // if (term) {
+    //   params.set("query", term);
+    // } else {
+    //   params.delete("query");
+    // }
+    // replace(`${pathname}?${params.toString()}`);
+  }
   return (
     <div className="flex items-center h-10 rounded-lg bg-gray-100 m-6 pl-3">
       <FontAwesomeIcon icon={faMagnifyingGlass} />
@@ -13,6 +28,9 @@ const Search = () => {
         type="text"
         placeholder="Search posts..."
         className="w-full h-full bg-transparent pl-2 outline-none"
+        onChange={(e) => {
+          handleSearch(e.target.value);
+        }}
       />
     </div>
   );
