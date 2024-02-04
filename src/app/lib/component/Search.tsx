cd857f171dae055651190b236efc0a9547d8fa8b
Anastasia <122746649+disturbiagirl@users.noreply.github.com>
2024-02-04 22:47:56 +0100

    passing query to the Home page

--- src/app/lib/component/Search.tsx
+++ src/app/lib/component/Search.tsx
@@ -2,6 +2,7 @@
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import { faMagnifyingGlass } from "@fortawesome/free-solid-svg-icons";
 import { useSearchParams, usePathname, useRouter } from "next/navigation";
+// import { useState } from "react";
 
 const Search = () => {
   const searchParams = useSearchParams();
@@ -9,6 +10,8 @@ const Search = () => {
   const { replace } = useRouter();
   function handleSearch(term: string) {
     const params = new URLSearchParams(searchParams);
+    // const [url, setUrl] = useState("");
+    // // setUrl(term);
     console.log(term);
 
     if (term) {
@@ -16,6 +19,7 @@ const Search = () => {
     } else {
       params.delete("query");
     }
+    console.log(params.toString());
     replace(`${pathname}?${params.toString()}`);
   }
   return (
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,8 +1,17 @@
 import Image from "next/image";
 import Posts from "./lib/component/Posts";
 import Search from "./lib/component/Search";
+import { SearchParamsContext } from "next/dist/shared/lib/hooks-client-context.shared-runtime";
 
-export default function Home() {
+export default function Home({
+  searchParams,
+}: {
+  searchParams?: {
+    query?: string;
+    page?: string;
+  };
+}) {
+  const query = searchParams?.query || "";
   return (
     <div className="w-11/12 md:w-10/12 lg:w-9/12 mx-auto">
       <div className="flex justify-center pl-6 pr-6">
@@ -23,8 +32,8 @@ export default function Home() {
         <div className="flex items-center justify-center">
           <Posts />
         </div>
-        S
       </div>
+      <p>{query}</p>
     </div>
   );
 }
