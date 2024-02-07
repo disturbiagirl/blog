fd19ef2035df11bba9fc5aeccd9bbf352a6ccc6b
Anastasia <122746649+disturbiagirl@users.noreply.github.com>
2024-02-07 21:19:13 +0100

    implementing useState for Search functionality

--- src/app/lib/component/Search.tsx
+++ src/app/lib/component/Search.tsx
@@ -1,27 +1,16 @@
 "use client";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import { faMagnifyingGlass } from "@fortawesome/free-solid-svg-icons";
-import { useSearchParams, usePathname, useRouter } from "next/navigation";
-// import { useState } from "react";
+import { useState } from "react";
 
 const Search = () => {
-  const searchParams = useSearchParams();
-  const pathname = usePathname();
-  const { replace } = useRouter();
-  function handleSearch(term: string) {
-    const params = new URLSearchParams(searchParams);
-    // const [url, setUrl] = useState("");
-    // // setUrl(term);
-    console.log(term);
+  const [searchTerm, setSearchTerm] = useState("");
 
-    if (term) {
-      params.set("query", term);
-    } else {
-      params.delete("query");
-    }
-    console.log(params.toString());
-    replace(`${pathname}?${params.toString()}`);
-  }
+  const handleChange = (e: string) => {
+    setSearchTerm(e);
+    console.log(searchTerm);
+  };
+
   return (
     <div className="flex items-center h-10 pl-3 m-6 bg-gray-100 rounded-lg">
       <FontAwesomeIcon icon={faMagnifyingGlass} />
@@ -30,9 +19,7 @@ const Search = () => {
         type="text"
         placeholder="Search posts..."
         className="w-full h-full pl-2 bg-transparent outline-none"
-        onChange={(e) => {
-          handleSearch(e.target.value);
-        }}
+        onChange={(e) => handleChange(e.target.value)}
       />
     </div>
   );
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,16 +1,8 @@
 import Image from "next/image";
 import Posts from "./lib/component/Posts";
 import Search from "./lib/component/Search";
-import { SearchParamsContext } from "next/dist/shared/lib/hooks-client-context.shared-runtime";
 
-export default function Home({
-  searchParams,
-}: {
-  searchParams?: {
-    query?: string;
-  };
-}) {
-  const query = searchParams?.query || "";
+export default function Home() {
   return (
     <div className="w-11/12 mx-auto md:w-10/12 lg:w-9/12">
       <div className="flex justify-center pl-6 pr-6">
@@ -26,9 +18,7 @@ export default function Home({
         />
       </div>
       <Search />
-      <div className="p-6">
-        <p>Search results: {query}</p>
-      </div>
+      <div className="p-6">{/* <p>Search results: {query}</p> */}</div>
       <div className="flex flex-col items-center justify-center p-6">
         <h2 className="pb-6 text-xl font-bold">Latest Blogs</h2>
         <div className="flex items-center justify-center">
